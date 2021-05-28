dc71d2ea2097911f4921a52f75309bfe2267e42e
Rohit Suryanarayan <53330475+rohitsuryanarayan@users.noreply.github.com>
2021-05-28 20:45:20 +0530

    Create Emotion Detection.py
    only face detection is implemented

---
+++ Emotion Detection.py
@@ -0,0 +1,32 @@
+import dlib
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread('image2.jpeg')
+frontalface_detector = dlib.get_frontal_face_detector()
+def rect_to_bb(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
+def detect_face(image_url):
+  try:
+    url_response = urllib.request.urlopen(image_url)
+    img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+    image = cv2.imdecode(img_array, -1)
+  except:
+    print('error/exception')
+
+#rects = frontalface_detector(image, 1)
+rects = frontalface_detector(image,1)
+if len(rects) < 1:
+  print("No Face Detected")
+for (i, rect) in enumerate(rects):
+  (x, y, w, h) = rect_to_bb(rect)
+  cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+plt.imshow(image, interpolation='nearest')
+plt.axis('off')
+plt.show()
